0d7296378663e7292b128622619f6da56ebbd8c9
Prem Vora <voraprem20@gmail.com>
2024-04-14 20:51:47 -0700

    Updated Profile Page UI

---
+++ client/src/components/ProfileNav.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { FaSignOutAlt  } from 'react-icons/fa';
+import '../styling/styles.css';
+
+export default function ProfileNav() {
+  return (
+    <div className='profilenav'>
+      <div className='application-nav'>
+        My Applications
+      </div>
+    </div>
+  )
+}
--- client/src/pages/ProfilePage.jsx
+++ client/src/pages/ProfilePage.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
+import ProfileNav from "../components/ProfileNav";
 
 export default function ProfilePage() {
   const iconList = ["home", "logout"];
   return (
     <>
       <Header headerTag={"Profile"} iconList={iconList} />
+      <ProfileNav />
       <Footer />
     </>
   );
