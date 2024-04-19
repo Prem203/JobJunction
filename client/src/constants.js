6ce4c702555abfeea56d945780c10e413c2f597c
Diya Wadhwani <wadhwani2399@gmail.com>
2024-04-18 22:52:59 -0700

    Updated landing page

--- client/src/constants.js
+++ client/src/constants.js
@@ -1,4 +1,4 @@
 export const ICON_LIST_PROFILE = ["home", "bookmarks", "logout"];
 export const ICON_LIST_COMMON = ["home"];
-export const ICON_LIST_HOME = ["login", "briefcase", "info", "user"];//user has to be removed
+export const ICON_LIST_HOME_LOGOUT = ["login", "briefcase", "info"];
 export const ICON_LIST_HOME_LOGIN = ["user", "briefcase", "info"];
